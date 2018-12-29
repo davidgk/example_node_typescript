047e2987681aec14434174b694afeecf8668413a
David Kotlirevsky <david.kotlirevsky@gmail.com>
2018-12-29 17:44:35 -0300

    Adding EJS and make it works
    Adding materialize
    Making it run with all stuff added in package json
    Running build.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import express = require("express");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 8085; // default port to listen
 
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-	res.send( "Hello world!" );
+    res.render( "index" );
 } );
 
 // start the Express server
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
-	console.log( `server started at http://localhost:${ port }` );
+	console.log ( `server started at http://localhost: ${ port } ` );
 } );
